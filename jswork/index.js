6d98d4b35324b1385e43ae6966d4b04421c0f043
priyanshu-royO5 <rockp.roy799@gmail.com>
2024-10-18 08:57:43 +0530

    Js work

---
+++ jswork/index.js
@@ -0,0 +1,15 @@
+// console.log("HUSN")
+
+// var a = 12
+// if (a>10){
+//   a = 23
+//   console.log("Age = "+a)
+// }
+
+function fun(a,b){
+  console.log("Kuch Bhi!!!")
+  return a+b
+}
+
+let ab=fun(5,7);
+console.log(ab)
